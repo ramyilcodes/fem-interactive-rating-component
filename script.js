d46b62f50cb345e6c690b4f3d5e2d6235df0e7df
Ramnan Ramyil <ramyilramnan@gmail.com>
2024-05-16 19:22:00 +0100

    centered card vertically

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const template = document.getElementById("template");
 
 const form = document.getElementById("form");
+
+const rating = document.querySelectorAll("rating");
+
+console.log(rating.values)
